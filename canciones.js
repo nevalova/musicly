a2340f1a9b8b2ba73d7837e70b51f8f5996036c0
nevalova <terrasnet1@gmail.com>
2026-04-11 16:11:54 -0600

    Add files via upload

---
+++ canciones.js
@@ -0,0 +1,12 @@
+// canciones.js
+const listaCancionesBase = [
+    { t: "September", a: "Earth, Wind & Fire", y: 1978, idYT: "Gs069dndIYk" },
+    { t: "Take On Me", a: "a-ha", y: 1984, idYT: "djV11Xbc914" },
+    { t: "Beat It", a: "Michael Jackson", y: 1982, idYT: "oRdxUFDoQe0" },
+    { t: "Toxic", a: "Britney Spears", y: 2003, idYT: "LOZuxwVk7TU" },
+    { t: "Uptown Funk", a: "Bruno Mars", y: 2014, idYT: "OPf0YbXqDm0" },
+    { t: "Rayando El Sol", a: "Maná", y: 1990, idYT: "tcWzVUlR7Y4" },
+    { t: "Gasolina", a: "Daddy Yankee", y: 2004, idYT: "qGKrc3Vz12g" },
+    { t: "Stayin Alive", a: "Bee Gees", y: 1977, idYT: "I_izvAbhExY" }
+    // Añade más aquí siguiendo el mismo formato
+];
