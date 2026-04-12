51574278122436a1e205454737b0e74424bc1368
nevalova <terrasnet1@gmail.com>
2026-04-11 22:25:05 -0600

    Add files via upload

--- canciones.js
+++ canciones.js
@@ -1,4 +1,3 @@
-// Agregando 20 más para un total de 30 (puedes llegar a 300+)
 const listaPrueba = [
     // --- Tus 10 originales ---
     { titulo: "El Triste", artista: "José José", year: 1970, youtubeId: "E20G25SCAEg" },
@@ -13,22 +12,22 @@ const listaPrueba = [
     { titulo: "La Nave del Olvido", artista: "José José", year: 1970, youtubeId: "BprZX_bGhhg" },
     { titulo: "Rayando el Sol", artista: "Maná", year: 1990, youtubeId: "tV_6V7780f0" },
     { titulo: "La Incondicional", artista: "Luis Miguel", year: 1988, youtubeId: "3u8Gf_N9M-Q" },
-    { titulo: "Persiana Americana", artista: "Soda Stereo", year: 1986, youtubeId: "mO_mZ0m637c" },
+    { titulo: "Persiana Americana", artista: "Soda Stereo", year: 1986, youtubeId: "7ZdLVrui8tM" }, // Actualizado
     { titulo: "Querida", artista: "Juan Gabriel", year: 1984, youtubeId: "o9v_R8mS4I0" },
     { titulo: "Vuela Vuela", artista: "Magneto", year: 1991, youtubeId: "T7XGz07D8_A" },
     { titulo: "Ciega, Sordomuda", artista: "Shakira", year: 1998, youtubeId: "mN9XvE7kGls" },
-    { titulo: "Lamento Boliviano", artista: "Enanitos Verdes", year: 1994, youtubeId: "khbDnLqe_Wk" },
+    { titulo: "Lamento Boliviano", artista: "Enanitos Verdes", year: 1994, youtubeId: "lYC5wlY8d6A" }, // Actualizado
     { titulo: "Provócame", artista: "Chayanne", year: 1992, youtubeId: "LpW6sX9x7O0" },
     { titulo: "Azul", artista: "Cristian Castro", year: 2001, youtubeId: "w_R_H_XN_XU" },
     { titulo: "Bidi Bidi Bom Bom", artista: "Selena", year: 1994, youtubeId: "R4V3Z29JK_o" },
-    { titulo: "Mariposa Tecknicolor", artista: "Fito Páez", year: 1994, youtubeId: "o6f_fW0pW_w" },
-    { titulo: "La Flaca", artista: "Jarabe de Palo", year: 1996, youtubeId: "r2g0pM3PMTo" },
+    { titulo: "Mariposa Tecknicolor", artista: "Fito Páez", year: 1994, youtubeId: "CRBincBc_8k" }, // Actualizado
+    { titulo: "La Flaca", artista: "Jarabe de Palo", year: 1996, youtubeId: "r2g0pM3PMNQ" }, // Actualizado
     { titulo: "Fiebre de Sábado", artista: "Los Tucanes de Tijuana", year: 1994, youtubeId: "L_Xid_Q9Cmc" },
     { titulo: "Música Ligera", artista: "Soda Stereo", year: 1990, youtubeId: "T_FkEw27XJ0" },
-    { titulo: "Si No Te Hubieras Ido", artista: "Marco Antonio Solís", year: 1999, youtubeId: "m3vWqNNoPzE" },
-    { titulo: "La Gasolina", artista: "Daddy Yankee", year: 2004, youtubeId: "qGKrc3Vz12g" },
-    { titulo: "Sabor a Mí", artista: "Luis Miguel", year: 1997, youtubeId: "aAn8U8iT7eE" },
-    { titulo: "Amante Bandido", artista: "Miguel Bosé", year: 1984, youtubeId: "N7xL7e-jM_0" },
-    { titulo: "Éxtasis", artista: "Cartel de Santa", year: 2012, youtubeId: "x_vXNfR43-Q" },
-    { titulo: "Rosa Pastel", artista: "Belanova", year: 2005, youtubeId: "H969Vf2-MHA" }
+    { titulo: "Si No Te Hubieras Ido", artista: "Marco Antonio Solís", year: 1999, youtubeId: "tV_6V7780f0" }, // Actualizado
+    { titulo: "La Gasolina", artista: "Daddy Yankee", year: 2004, youtubeId: "GJP9ZTsgxJs" }, // Actualizado
+    { titulo: "Sabor a Mí", artista: "Luis Miguel", year: 1997, youtubeId: "cNo-d6Y6w8Y" }, // Actualizado
+    { titulo: "Amante Bandido", artista: "Miguel Bosé", year: 1984, youtubeId: "smfo5w7sKMY" }, // Actualizado
+    { titulo: "Éxtasis", artista: "Cartel de Santa", year: 2012, youtubeId: "WB58k5bDFp8" }, // Actualizado
+    { titulo: "Rosa Pastel", artista: "Belanova", year: 2005, youtubeId: "bUkJLkoh3kA" }  // Actualizado
 ];
