51b06f69bd3d24913425cb4989f562bc9525404b
nevalova <terrasnet1@gmail.com>
2026-04-15 13:09:38 -0600

    Add files via upload

--- canciones.js
+++ canciones.js
@@ -1,33 +1,12 @@
-const listaPrueba = [
-    // --- Tus 10 originales ---
-    { titulo: "El Triste", artista: "José José", year: 1970, youtubeId: "E20G25SCAEg" },
-    { titulo: "Amor Eterno", artista: "Juan Gabriel", year: 1984, youtubeId: "RgKqxLAhRKE" },
-    { titulo: "Cien Años", artista: "Pedro Infante", year: 1953, youtubeId: "1Y9Sy1e2CFY" },
-    { titulo: "El Rey", artista: "Vicente Fernández", year: 1971, youtubeId: "zhHrefOdlLs" },
-    { titulo: "Tragos Amargos", artista: "Ramón Ayala", year: 1980, youtubeId: "3hpGnaujENM" },
-    { titulo: "La Puerta Negra", artista: "Los Tigres del Norte", year: 1986, youtubeId: "rryDND06LHU" },
-    { titulo: "Como la Flor", artista: "Selena", year: 1992, youtubeId: "FwZTgDjRLM0" },
-    { titulo: "Afuera", artista: "Caifanes", year: 1994, youtubeId: "gBt1jOtKz6Y" },
-    { titulo: "Eres", artista: "Café Tacvba", year: 2003, youtubeId: "98Akpf1ph2o" },
-    { titulo: "La Nave del Olvido", artista: "José José", year: 1970, youtubeId: "BprZX_bGhhg" },
-    { titulo: "Rayando el Sol", artista: "Maná", year: 1990, youtubeId: "tV_6V7780f0" },
-    { titulo: "La Incondicional", artista: "Luis Miguel", year: 1988, youtubeId: "3u8Gf_N9M-Q" },
-    { titulo: "Persiana Americana", artista: "Soda Stereo", year: 1986, youtubeId: "7ZdLVrui8tM" }, // Actualizado
-    { titulo: "Querida", artista: "Juan Gabriel", year: 1984, youtubeId: "o9v_R8mS4I0" },
-    { titulo: "Vuela Vuela", artista: "Magneto", year: 1991, youtubeId: "T7XGz07D8_A" },
-    { titulo: "Ciega, Sordomuda", artista: "Shakira", year: 1998, youtubeId: "mN9XvE7kGls" },
-    { titulo: "Lamento Boliviano", artista: "Enanitos Verdes", year: 1994, youtubeId: "lYC5wlY8d6A" }, // Actualizado
-    { titulo: "Provócame", artista: "Chayanne", year: 1992, youtubeId: "LpW6sX9x7O0" },
-    { titulo: "Azul", artista: "Cristian Castro", year: 2001, youtubeId: "w_R_H_XN_XU" },
-    { titulo: "Bidi Bidi Bom Bom", artista: "Selena", year: 1994, youtubeId: "R4V3Z29JK_o" },
-    { titulo: "Mariposa Tecknicolor", artista: "Fito Páez", year: 1994, youtubeId: "CRBincBc_8k" }, // Actualizado
-    { titulo: "La Flaca", artista: "Jarabe de Palo", year: 1996, youtubeId: "r2g0pM3PMNQ" }, // Actualizado
-    { titulo: "Fiebre de Sábado", artista: "Los Tucanes de Tijuana", year: 1994, youtubeId: "L_Xid_Q9Cmc" },
-    { titulo: "Música Ligera", artista: "Soda Stereo", year: 1990, youtubeId: "T_FkEw27XJ0" },
-    { titulo: "Si No Te Hubieras Ido", artista: "Marco Antonio Solís", year: 1999, youtubeId: "tV_6V7780f0" }, // Actualizado
-    { titulo: "La Gasolina", artista: "Daddy Yankee", year: 2004, youtubeId: "GJP9ZTsgxJs" }, // Actualizado
-    { titulo: "Sabor a Mí", artista: "Luis Miguel", year: 1997, youtubeId: "cNo-d6Y6w8Y" }, // Actualizado
-    { titulo: "Amante Bandido", artista: "Miguel Bosé", year: 1984, youtubeId: "smfo5w7sKMY" }, // Actualizado
-    { titulo: "Éxtasis", artista: "Cartel de Santa", year: 2012, youtubeId: "WB58k5bDFp8" }, // Actualizado
-    { titulo: "Rosa Pastel", artista: "Belanova", year: 2005, youtubeId: "bUkJLkoh3kA" }  // Actualizado
+const CANCIONES = [
+    { titulo: "Blitzkrieg Bop", artista: "Ramones", year: 1976, spotifyId: "4KcH1ZRV2W1q7Flq0QqC76" },
+    { titulo: "En algún lugar", artista: "Duncan Dhu", year: 1987, spotifyId: "3UIENhLRdFIOuRan92cAQu" },
+    { titulo: "One More Time", artista: "Daft Punk", year: 2001, spotifyId: "2Uy6EhQXAYkXA6MohPgjpV" },
+    { titulo: "México Lindo y Querido", artista: "Jorge Negrete", year: 1952, spotifyId: "567aGQVtUEhNuDpp9Dtewp" },
+    { titulo: "Tequila", artista: "The Champs", year: 1958, spotifyId: "5gJKsGij5oGt5H5RSFYXPa" },
+    { titulo: "Regalo de Dios", artista: "Julión Alvares y su Norteño Banda", year: 2023, spotifyId: "51qYdMBxt9mFbtZBzq1Yfi" },
+    { titulo: "Oblivion", artista: "Grimes", year: 2012, spotifyId: "4BY4V1T0RJK1HmD2Q0ClyK" },
+    { titulo: "Piano Man", artista: "Billy Joel", year: 1973, spotifyId: "78WVLOP9pN0G3gRLFy1rAa" },
+    { titulo: "We Own the Night", artista: "Dance Gavin Dance", year: 2015, spotifyId: "7xCcUcMcGsIYGKUVgBZUw5" },
+    { titulo: "Contrabando y Traición", artista: "Los Tigres del Norte", year: 1974, spotifyId: "4gRi0itWso2UodoRMP5asJ" }
 ];
